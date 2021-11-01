1fca8f70c508e04aa0e07e3bbe483f2dc6809a78
wilsonzwl <857534648@qq.com>
2021-11-01 19:28:30 +0800

    first commit

---
+++ src/main/java/com/shengsiyuan/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.shengsiyuan.git;
+
+/**
+ * @Classname GitTest
+ * @Description 类型说明
+ * @Date 2021/11/1 19:18
+ * @Created by zhangwenliang
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
